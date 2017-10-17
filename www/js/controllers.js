11d294f4bb60345b9b8a03608e1d82a6a9c9bd8d
joel <joelunmsm@gmail.com>
2017-10-17 14:35:19 -0500

    cambios

---
+++ www/js/controllers.js
@@ -0,0 +1,61 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+    $scope.ola ='sjsjs'
+
+}])
+   
+.controller('capitalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('sLARELEVANTESCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+    
+$scope.data = 99999
+
+
+}])
+   
+.controller('puestosYAgentesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('puestosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('informacionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('inicioCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,50 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+    .state('login', {
+      url: '/page1',
+      templateUrl: 'templates/login.html'
+    })
+        .state('capital', {
+      url: '/side-menu21',
+      templateUrl: 'templates/capital.html'
+    })
+        .state('capital.sLARELEVANTES', {
+      url: '/page11',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/sLARELEVANTES.html'
+        }
+      }
+    })
+        .state('puestosYAgentes', {
+      url: '/page35',
+      templateUrl: 'templates/puestosYAgentes.html'
+    })
+        .state('puestos', {
+      url: '/page36',
+      templateUrl: 'templates/puestos.html'
+    })
+        .state('informacion', {
+      url: '/page37',
+      templateUrl: 'templates/informacion.html'
+    })
+        .state('inicio', {
+      url: '/page30',
+      templateUrl: 'templates/inicio.html'
+    })
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page1');
+
+
+});
