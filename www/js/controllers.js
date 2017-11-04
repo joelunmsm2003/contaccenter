bd818c158565d1a7ae49b6afb84fbccfaa699323
root <root@sun>
2017-11-04 11:31:43 -0500

    graficas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,14 +37,31 @@ function ($scope, $stateParams,$http,$timeout ) {
       },
 
       xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-        ]
-      },
+        categories: ['Africa']
+   
+    },
 
-      series: [{
-        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-      }]
+    yAxis: {
+        title: {
+            text: 'Valores expresados en porcentage'
+        }
+    },
+      
+
+    series: [{
+        name: 'Abandono',
+        data: [107]
+    }, {
+        name: 'Ocupacion',
+        data: [133]
+    }, {
+        name: 'Nivel de Servicio',
+        data: [1052]
+    }]
+
+
+
+
     });
 
 
